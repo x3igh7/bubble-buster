6514f4940f0ba439434f7a160e6f8abcf010f2da
Conner Smith <connerpsmith@gmail.com>
2017-02-25 16:04:53 -0600

    code to return json tweets by our selected tweeters

--- BubbleBuster/BubbleBuster/BubbleBuster/Function.cs
+++ BubbleBuster/BubbleBuster/BubbleBuster/Function.cs
@@ -1,5 +1,15 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
 using Amazon.Lambda.Core;
-using Amazon.Lambda.Serialization.Json;
+using Amazon.Lambda.Model;
+using Amazon.S3.Model;
+using Newtonsoft.Json;
+using Tweetinvi;
+using Tweetinvi.Models;
+using Tweetinvi.Parameters;
+using Environment = System.Environment;
+using JsonSerializer = Amazon.Lambda.Serialization.Json.JsonSerializer;
 
 // Assembly attribute to enable the Lambda function's JSON input to be converted into a .NET class.
 
@@ -15,9 +25,29 @@ namespace BubbleBuster
         /// <param name="input"></param>
         /// <param name="context"></param>
         /// <returns></returns>
-        public string FunctionHandler(string input, ILambdaContext context)
+        public string FunctionHandler(ILambdaContext context)
         {
-            return input?.ToUpper();
+            var consumerKey = Environment.GetEnvironmentVariable("ConsumerKey");
+            var consumerSecret = Environment.GetEnvironmentVariable("ConsumerSecret");
+            var accessToken = Environment.GetEnvironmentVariable("AccessToken");
+            var accessTokenSecret = Environment.GetEnvironmentVariable("AccessTokenSecret");
+
+            Auth.SetUserCredentials(consumerKey, consumerSecret, accessToken, accessTokenSecret);
+
+            //var currentUser = User.GetAuthenticatedUser();
+            //var users = currentUser.GetUsersYouRequestedToFollow();
+            //return JsonConvert.SerializeObject(currentUser);
+
+            var timelines = new Dictionary<string, IEnumerable<ITweet>>();
+
+            var followed = new List<string> {"FoxNews", "CNN", "MSNBC"};
+            foreach (var screename in followed)
+            {
+                var recentTweets = Timeline.GetUserTimeline(screename, 40);
+                timelines.Add(screename, recentTweets);
+            }
+
+            return JsonConvert.SerializeObject(timelines);
         }
     }
 }
